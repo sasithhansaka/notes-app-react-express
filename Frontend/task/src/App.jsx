f48e47f483522ce29b0df7c82ad9932bd34dc670
sasithhansaka <sasithhansaka285@gmail.com>
2025-03-27 12:07:49 +0530

    update routing in App component; add NotFound page and improve code formatting; set default font in index.css

--- Frontend/task/src/App.jsx
+++ Frontend/task/src/App.jsx
@@ -1,22 +1,23 @@
-import './App.css'
-import RegisterPage from './Pages/RegisterPage'
-import LoginPage from './Pages/LoginPage'
-import Profile from './Pages/Profile'
+import "./App.css";
+import RegisterPage from "./Pages/RegisterPage";
+import LoginPage from "./Pages/LoginPage";
+import Profile from "./Pages/Profile";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home'
+import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  
   return (
     <Router>
       <Routes>
-      <Route path='/' element={<LoginPage/>}></Route>
-        <Route path='/dashboard' element={<Home/>}></Route>
-        <Route path='/register' element={<RegisterPage/>}></Route>
-        <Route path='/profile' element={<Profile/>}></Route>
+        <Route index element={<LoginPage />}></Route>
+        <Route path="/dashboard" element={<HomePage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
